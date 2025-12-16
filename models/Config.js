5d92a989fa33f2668a7be02b5dad2dedbe6bcef4
mdsultanmahmud416-png <mdsultanmahmud416@gmail.com>
2025-12-16 21:52:01 +0600

    Update Config.js

--- models/Config.js
+++ models/Config.js
@@ -1,12 +1,17 @@
 // ConfigSchema বা models Config => Config folder 
 const mongoose = require("mongoose");
 
-const ConfigSchema = new mongoose.Schema({
+const ConfigSchema = new mongoose.Schema(
+  {
   key: { type: String, unique: true },
   value: Object,
   updatedAt: { type: Date, default: Date.now }
-});
+},
+  {
+    collection: "Config" // 👈 EXACT collection name
+  }
+);
 
-module.exports =
-  mongoose.models.Config ||
+module.exports = mongoose.models.Config ||
   mongoose.model("Config", ConfigSchema);
+
